4c92d9361dffda7ba9c73ba49c8dc278f7e36540
vovaf88 <vovaf88@mail.ru>
2023-11-26 19:10:25 +0300

    2 commit add models

--- service/service/settings.py
+++ service/service/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+
+    'clients',
+    'services',
 ]
 
 MIDDLEWARE = [
--- service/services/admin.py
+++ service/services/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from services.models import *
 
-# Register your models here.
+
+admin.site.register(Service)
+admin.site.register(Plan)
+admin.site.register(Subscription)
+
+
+
+
+
+
+
+
+
