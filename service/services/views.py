db3e4b67501b5ef4bb79e306050714dd18c895f5
vovaf88 <vovaf88@mail.ru>
2023-11-29 19:30:05 +0300

    7 commit sum

--- service/services/views.py
+++ service/services/views.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from services.models import *
 from services.serializers import *
 from clients.models import Client
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
@@ -11,6 +11,19 @@ class SubscriptionView(ReadOnlyModelViewSet):
         'plan',
         Prefetch('client',
                  queryset=Client.objects.all().select_related('user').only('company_name',
-                                                              'user__email')))
+                                                              'user__email'))).annotate(price=
+                                                            F('service__full_price') -
+                                                            F('service__full_price') *
+                                                            F('plan__discount_percent')/100.00)
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        responce = super().list(request, *args, **kwargs)
+
+        responce_data = {'result': responce.data}
+        responce_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        responce.data = responce_data
+
+        return responce
 
